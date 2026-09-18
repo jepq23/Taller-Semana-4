c170323774aad34519bef5e6f247dc2d988bfa51
Carlosz456 <karloszp001@gmail.com>
2026-09-17 19:42:02 -0500

    Codigo de enemigo Range

--- Assets/Scripts/EnemigoRange.cs
+++ Assets/Scripts/EnemigoRange.cs
@@ -2,15 +2,60 @@ using UnityEngine;
 
 public class EnemigoRange : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Referencias")]
+    [SerializeField] private Transform jugador;
+    [SerializeField] private Transform puntoDisparo;
+    [SerializeField] private GameObject prefabProyectil;
+
+    [Header("Disparo")]
+    [SerializeField] private float rangoAtaque = 8f;
+    [SerializeField] private float cadenciaDisparo = 1.5f;
+    [SerializeField] private float velocidadProyectil = 20f;
+
+    private float temporizadorDisparo;
+
+    private void Awake()
     {
-        
+        if (jugador == null)
+        {
+            GameObject objJugador = GameObject.FindGameObjectWithTag("Player");
+            if (objJugador != null) jugador = objJugador.transform;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (jugador == null) return;
+
+        float distancia = Vector3.Distance(transform.position, jugador.position);
+
+        if (distancia <= rangoAtaque)
+        {
+            temporizadorDisparo -= Time.deltaTime;
+            if (temporizadorDisparo <= 0f)
+            {
+                Disparar();
+                temporizadorDisparo = cadenciaDisparo;
+            }
+        }
+    }
+
+    private void Disparar()
+    {
+        if (prefabProyectil == null || puntoDisparo == null) return;
+
+        Vector3 direccionDisparo = (jugador.position - puntoDisparo.position).normalized;
+        GameObject proyectil = Instantiate(prefabProyectil, puntoDisparo.position, Quaternion.LookRotation(direccionDisparo));
+
+        if (proyectil.TryGetComponent<Rigidbody>(out Rigidbody rb))
+        {
+            rb.velocity = direccionDisparo * velocidadProyectil;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rangoAtaque);
     }
 }
